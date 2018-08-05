b7c96b70914a5c95ca336eb1eec0a4a6b688d2e3
efeng1st <efeng1st@gmail.com>
2018-08-05 17:26:00 +0800

    继续组件学习

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
+// 定义一个 React 组件
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      // jsx 语法
+      <div>
         Hello, I am efeng1st.
       </div>
     );
