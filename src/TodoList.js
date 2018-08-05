132ecce8a00f615ff729b491db77165fc3698084
efeng1st <efeng1st@gmail.com>
2018-08-05 21:18:29 +0800

    React 中组件的拆分

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,28 +1,29 @@
 import React, { Component } from "react";
+import TodoItem from "./TodoItem";
 
 class TodoList extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       list: [],
-      inputValue: ''
-    }
+      inputValue: ""
+    };
   }
 
   handleBtnClick() {
     this.setState({
       list: [...this.state.list, this.state.inputValue],
-      inputValue: ''
-    })
+      inputValue: ""
+    });
   }
 
   handleInputChange(e) {
     this.setState({
       inputValue: e.target.value
-    })
+    });
   }
 
+<<<<<<< HEAD
   handleItemClick(index){
     const list = [...this.state.list];
     list.splice(index, 1);
@@ -30,19 +31,41 @@ class TodoList extends Component {
     this.setState({list})
   }
 
+=======
+  handleItemClick(index) {
+    const list = [...this.state.list];
+    list.splice(index, 1);
+
+    this.setState({ list });
+  }
+
+  // 父组件通过属性的形式向子组件传递参数
+  // 子组件通过procs接收父组件传递过来的参数
+
+>>>>>>> React 中组件的拆分
   render() {
     return (
       <div>
         <div>
-          <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
+          <input
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />
           <button onClick={this.handleBtnClick.bind(this)}>Add</button>
         </div>
         <ul>
+<<<<<<< HEAD
           {
             this.state.list.map((item, index) => {
               return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
             })
           }
+=======
+          {this.state.list.map((item, index) => {
+            return <TodoItem content={item} key={index}/>;
+            //return <li key={index} onClick={this.handleItemClick.bind(this, index)}>{item}</li>
+          })}
+>>>>>>> React 中组件的拆分
         </ul>
       </div>
     );
